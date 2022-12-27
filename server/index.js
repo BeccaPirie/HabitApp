c0e0691f07442ed9e68e7f0063646de568c0983a
BeccaPirie <43371064+BeccaPirie@users.noreply.github.com>
2022-12-27 21:11:12 +0000

    use mongoose

--- server/index.js
+++ server/index.js
@@ -1,20 +1,17 @@
 import app from './server.js'
-import mongodb from 'mongodb'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 async function main() {
     dotenv.config()
-
-    const client = new mongodb.MongoClient(process.env.DATABASE_URI,
-        {useNewUrlParser: true, useUnifiedTopology: true})
     const port = 5000 || 8000
 
     try {
-        await client.connect()
-
+        await mongoose.connect(process.env.DATABASE_URI,
+            { useNewUrlParser: true, useUnifiedTopology: true});
         app.listen(port, ()=> {
-            console.log('listening on port ' + port)
-        })
+                console.log(`listening on port: ${port}`)
+            })
     } catch (err) {
         console.error(err)
         process.exit(1)
