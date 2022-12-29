0687ac9d04d43f70ee343776bd435b80ef1de03d
BeccaPirie <43371064+BeccaPirie@users.noreply.github.com>
2022-12-29 15:42:43 +0000

    user auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 
@@ -6,12 +7,39 @@ router.get('/', async(req, res) => {res.send("auth route")})
 
 // register
 router.post('/signup', async(req, res) => {
+    try {
+        // create password
+        const bcryptPass = await bcrypt.genSalt(10)
+        const password = await bcrypt.hash(req.body.password, bcryptPass)
 
+        // create user
+        const createUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: password
+        })
+
+        // save user
+        const user = await createUser.save()
+        res.json(user)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 // login
 router.post('/login', async(req, res) => {
+    try {
+        // find user
+        const loginUser = await User.findOne({username: req.body.username})
+        if(!loginUser) res.status(404).json("User not found")
 
+        // check password
+        const loginPassword = await bcrypt.compare(loginUser.password, req.body.password)
+        if(!loginPassword) res.status(400).json("Incorrect password")
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 export default router
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import Habit from '../models/Habit.js'
 import User from '../models/User.js'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 
@@ -8,21 +9,52 @@ router.get('/', async(req, res) => {res.send("user route")})
 
 // update user
 router.put('/:id', async(req, res) => {
-    try {
+    // only update own account
+    if(req.body.id == req.params.id) {
+       try {
         await User.findByIdAndUpdate(req.params.id, {
             $set: {
                 username: req.body.username,
                 email: req.body.email
             }
         })
-    } catch (err) {
-        res.status(500).json(err)
+        } catch (err) {
+            res.status(500).json(err)
+        } 
+    } else {
+        res.status(403).json("You can only update your own account")
     }
+    
 })
 
 // update password
 router.put('/:id/update-password', async(req, res) => {
+    // only update own account
+    if(req.body.id === req.params.id) {
+        try {
+        const user = await User.findById(req.params.id)
+        const password = await bcrypt.compare(req.body.password, user.password)
 
+        if(!password) {
+            res.status(400).json("Incorrect password")
+        }
+        else {
+            // create new password
+            const bcryptPass = await bcrypt.genSalt(10)
+            const newPassword = await bcrypt.hash(req.body.newPassword, bcryptPass)
+        
+            // update user account with new password
+            await User.findByIdAndUpdate(req.params.id, {
+                $set: {password: newPassword}
+            })
+        }
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json("You can only update your own account")
+    }
+    
 })
 
 // delete user
